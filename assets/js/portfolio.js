e01fa49d0312040e4462f670631abf1f285ebd75
Viet Anh (Connor) Phung <pvaconnor@gmail.com>
2024-10-28 02:17:41 -0400

    fixing memory issues

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,21 +1,20 @@
+$(document).ready(function() {
+    const $galleryImages = $(".gallery .image");
+    const $activeButtonClass = "active";
 
-    $(document).ready(function() {
-        // Set default filter to "Projects"
-        $(".gallery .image").hide(); // Hide all items initially
-        $(".gallery .project").show(); // Show only "Projects" items
-    
-        // Filter functionality on button click
-        $(".button").click(function() {
-            $(this).addClass("active").siblings().removeClass("active");
-    
-            var filter = $(this).attr("data-filter");
-    
-            $(".gallery .image").hide(); // Hide all images
-            $(".gallery .image").filter("." + filter).show(); // Show only selected category
-        });
-    
+    // Set default filter to "Projects"
+    $galleryImages.hide(); // Hide all items initially
+    $(".gallery .project").show(); // Show only "Projects" items
 
+    // Filter functionality on button click
+    $(".button").click(function() {
+        const $this = $(this);
+        const filter = $this.attr("data-filter");
 
+        // Update active button class
+        $this.addClass($activeButtonClass).siblings().removeClass($activeButtonClass);
 
-
+        // Show and hide images based on the filter
+        $galleryImages.hide().filter("." + filter).show(); // Hide all, then show filtered
+    });
 });
